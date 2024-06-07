4ec0a176f3c0b075b2782f4e126992f3181eafe4
Bright <quanliang@dayuwuxian.com>
2024-06-07 18:31:16 +0800

    submit

--- MyApplication3/app/src/main/java/com/example/myapplication3/MainActivity.java
+++ MyApplication3/app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -7,39 +7,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 import androidx.viewbinding.ViewBinding;
 
+import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
 import com.example.myapplication3.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    private ViewPagerAdapter viewPagerAdapter;
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        BottomNavigationView bottomNavigationView = binding.bottomNavigationView;
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(binding.navHostFragmentContainer.getId());
 
-        viewPagerAdapter = new ViewPagerAdapter(this);
-        binding.viewPager.setAdapter(viewPagerAdapter);
-        new TabLayoutMediator(binding.tabLayout,
-                binding.viewPager,
-                (tab, i) -> {
-                    switch (i) {
-                        case 0:
-                            tab.setText("tab0");
-                            break;
-                        case 1:
-                            tab.setText("tab1");
-                            break;
-                        case 2:
-                            tab.setText("tab2");
-                            break;
-                    }
-                });
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
     }
 }
